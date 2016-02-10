ceef53987921627d28916f3acdae49983095c83a
Chris Bowdon <c.bowdon@bath.edu>
2016-02-10 23:25:23 +0000

    Add call to sodium-init

--- src/passable/core.clj
+++ src/passable/core.clj
@@ -1,33 +1,12 @@
 (ns passable.core
-  (:require [passable.crypto :refer :all]
+  (:require [caesium.core :refer [sodium-init]]
+            [passable.crypto :refer :all]
             [passable.io :refer :all]
             [passable.serialization :refer :all])
   (:gen-class))
 
-;; (def example
-;;   {:pass (.getBytes "hello")
-;;    :user (.getBytes "world")
-;;    :site "example.com"
-;;    :notes (.getBytes "foobarbaz")})
-;;
-;; (def secret-key (generate-secret-key))
-;; (def nonce (generate-nonce))
-;;
-;; (defn demo []
-;;   (let [f "/Users/chris/Downloads/test.dat"]
-;;     (->> example
-;;          (serialize)
-;;          (secretbox/encrypt secret-key nonce)
-;;          (hexify)
-;;          (spit f))))
-;; (defn demo2 []
-;;   (let [f "/Users/chris/Downloads/test.dat"]
-;;     (->> (slurp f)
-;;          (unhexify)
-;;          (secretbox/decrypt secret-key nonce)
-;;          (deserialize))))
-
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
+  (sodium-init)
   (printf "hello werld %s" args))
